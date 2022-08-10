6e9e8578654526ff8539f15f5e968234656756e2
Tiago Oliveira <tiagooliveira@takethewind.com>
2022-08-10 17:04:37 +0100

    [Add]Google Fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,11 @@
-import Document from 'next/document';
+import Document, { Head } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  return() {
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet"/>
+    </Head>
+  }
 }
